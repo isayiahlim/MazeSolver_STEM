aa91ff2d0307928f2bc4e632aba9d98aea62b8be
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2023-02-08 20:53:07 -0800

    Changed conditionals to check if NOT visited and added comments

--- MazeSolver/src/MazeGenerator.java
+++ MazeSolver/src/MazeGenerator.java
@@ -15,22 +15,29 @@ public class MazeGenerator
      */
     public Maze generate(int size)
     {
-        Maze maze = new Maze(size);
+        //generates maze with all walls in place
+    	Maze maze = new Maze(size);
+    	//stack used to iterate through cells
         Stack<Cell> mazeStack = new Stack<Cell>();
         mazeStack.push(new Cell(0,0));
-        int unvisited = 0;
+        //for every cell in the maze
         while(!mazeStack.isEmpty())
         {
-        	
+        	//pops and visits the cell
         	Cell current = mazeStack.pop();
         	int x = current.getX();
         	int y = current.getY();
         	maze.visit(x, y);
-        	//possible directions to go
+        	
+        	//possible directions to go & integer of how many directions are available
         	Direction[] directions = new Direction[4];
-        	if(x > 0)
+            int unvisited = 0;
+            
+        	//ensures a border wall is not removed then checks if the wall has already been visited
+            //for all four cardinal directions
+            if(x > 0)
         	{
-        		if(maze.isVisited(x-1, y))
+        		if(!maze.isVisited(x-1, y))
         		{
         			directions[unvisited] = Direction.LEFT;
         			unvisited ++;
@@ -38,7 +45,7 @@ public class MazeGenerator
         	}
         	if(x < size-1)
         	{
-        		if(maze.isVisited(x+1, y))
+        		if(!maze.isVisited(x+1, y))
         		{
         			directions[unvisited] = Direction.RIGHT;
         			unvisited ++;
@@ -46,7 +53,7 @@ public class MazeGenerator
         	}
         	if(y > 0)
         	{
-        		if(maze.isVisited(x, y-1))
+        		if(!maze.isVisited(x, y-1))
         		{
         			directions[unvisited] = Direction.DOWN;
         			unvisited ++;
@@ -54,17 +61,21 @@ public class MazeGenerator
         	}
         	if(y < size-1)
         	{
-        		if(maze.isVisited(x, y+1))
+        		if(!maze.isVisited(x, y+1))
         		{
         			directions[unvisited] = Direction.UP;
         			unvisited ++;
         		}
         	}
+        	
+        	//if there are any directions that haven't been visited, this randomly chooses one
+        	//and removes the wall in that direction
         	if(unvisited > 0)
         	{
 	        	Direction temp = directions[(int)(Math.random()*unvisited)];
 	        	maze.removeWall(x, y, temp);
 	        	mazeStack.push(current);
+	        	//adjusts the x and y according to the random direction chosen
 	        	if(temp == Direction.LEFT)
 	        		x --;
 	        	if(temp == Direction.RIGHT)
@@ -73,23 +84,25 @@ public class MazeGenerator
 	        		y--;
 	        	if(temp == Direction.UP)
 	        		y++;
+	        	//adds it to the stack
 	        	mazeStack.push(new Cell(x, y));
         	}
         }
         
-        //random start stop points
+        //random start & stop points, repeating to make sure they are different
         int startX = (int)(Math.random() * size);
         int startY = (int)(Math.random() * size);
         int endX = (int)(Math.random() * size);
         int endY = (int)(Math.random() * size);
-        do 
+        while ((startX == endX) && (startY == endY))
         {
             startX = (int)(Math.random() * size);
             startY = (int)(Math.random() * size);
             endX = (int)(Math.random() * size);
             endY = (int)(Math.random() * size);
-        } while ((startX == endX) && (startY == endY));
-
+        } 
+        
+        //sets the start and stop to these coordinates
         maze.setStart(startX, startY);
         maze.setEnd(endX, endY);
         return maze;
