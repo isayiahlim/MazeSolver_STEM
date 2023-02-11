760698f552342f51aa6862c70b81a0b75ee9b7c3
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2023-02-10 19:09:30 -0800

    Fixed some minor errors by storing the path taken instead of NoPath

--- MazeSolver/src/MazeSolver.java
+++ MazeSolver/src/MazeSolver.java
@@ -15,42 +15,42 @@ public class MazeSolver
      * @param maze the maze to solve
      * @return a solution for the maze or {@link Path#NO_PATH} if there is no solution
      */
-	//new object storing the path taken to get to every cell
+	//object storing a path taken to get to every cell
     public class StoredCell
     {
-    	 private Cell current;
-         private Path stored;
+    	//variables for current cell and the path to that cell 
+    	private Cell current;
+        private Path stored;
          
-         //stores the current cell and generates the path to get there
-         public StoredCell(Cell current, Path path)
-         {
-             this.current = current;
-             stored = new Path();
-             for(Cell cell : path)
-             {
-                 stored.addLast(cell);
-             }
-             stored.addLast(current);
-         }
-
-         public Cell getCell() 
-         { 
-        	 return current; 
-         }
-         public Path getPath() 
-         { 
-        	 return stored; 
-         }
+        public StoredCell(Cell current, Path path)
+        {
+            this.current = current;
+            //copies the path to the parameter cell & adds the new cell to the end
+            stored = new Path();
+            for(Cell cell : path)
+            {
+                stored.addLast(cell);
+            }
+            stored.addLast(current);
+        }
+        //methods to access vars
+        public Cell getCell() 
+        { 
+        	return current; 
+        }
+        public Path getPath() 
+        { 
+        	return stored; 
+        }
     }
 	
 	public Path solve(Maze maze)
     {
-        Path path = Path.NO_PATH;
-        //queue used to iterate through cells
+        //queue storing all cells & their paths
         Queue<StoredCell> pathStack = new Queue<StoredCell>();
-        pathStack.enqueue(new StoredCell(maze.getStart(), path));
+        pathStack.enqueue(new StoredCell(maze.getStart(), Path.NO_PATH));
         
-        //for every cell in the maze
+        //for every storedCell (cell + path) in the maze
         while(!pathStack.isEmpty())
         {
         	//pops and visits the cell
@@ -59,46 +59,45 @@ public class MazeSolver
         	int y = current.getCell().getY();
         	maze.visit(x, y);
         	
+        	//if the cell reaches the end, returns the path stored for whatever cell is found
         	if(current.getCell().equals(maze.getEnd()))
         	{
         		return current.getPath();
         	}
         	
         	//checks if a direction is open and unvisited for all cardinal directions
+        	//if it is, enqueues both the cell and the path to get to that cell
             if(x > 0)
         	{
         		if(!maze.isVisited(x-1, y) && maze.isOpen(x, y, Direction.LEFT))
         		{
-        			StoredCell tempC = new StoredCell(new Cell(x-1, y), Path.NO_PATH);
-        			pathStack.enqueue(tempC);
+        			pathStack.enqueue(new StoredCell(new Cell(x-1, y), current.getPath()));
         		}
         	}
         	if(x < maze.size()-1)
         	{
         		if(!maze.isVisited(x+1, y) && maze.isOpen(x, y, Direction.RIGHT))
         		{
-        			StoredCell tempC = new StoredCell(new Cell(x+1, y), Path.NO_PATH);
-        			pathStack.enqueue(tempC);
+        			pathStack.enqueue(new StoredCell(new Cell(x+1, y), current.getPath()));
         		}
         	}
         	if(y > 0)
         	{
-        		if(!maze.isVisited(x, y+1) && maze.isOpen(x, y, Direction.DOWN))
+        		if(!maze.isVisited(x, y-1) && maze.isOpen(x, y, Direction.DOWN))
         		{
-        			StoredCell tempC = new StoredCell(new Cell(x, y-1), Path.NO_PATH);
-        			pathStack.enqueue(tempC);
+        			pathStack.enqueue(new StoredCell(new Cell(x, y-1), current.getPath()));
         		}
         	}
         	if(y < maze.size()-1)
         	{
         		if(!maze.isVisited(x, y+1) && maze.isOpen(x, y, Direction.UP))
         		{
-        			StoredCell tempC = new StoredCell(new Cell(x, y+1), Path.NO_PATH);
-        			pathStack.enqueue(tempC);
+        			pathStack.enqueue(new StoredCell(new Cell(x, y+1), current.getPath()));
         		}
         	}
         	
         }
+        //if no path is returned, there is no solution
         return Path.NO_PATH;
     }
 
